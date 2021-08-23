4b451b338fb7a5ce3e67de08df8ca4c2a856ee5a
Rafael Molla <rafaelmolladouglas@gmail.com>
2021-08-23 18:52:02 -0300

    Class names changed and comments were added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Get reference to the password input
-const passwordEl = document.querySelector(".form__input--password");
+const passwordEl = document.querySelector(".form-input--password");
 
 // Get reference to the container for the eye image
 const eyeContainerEl = document.querySelector(".password-eye-container");
@@ -7,21 +7,21 @@ const eyeContainerEl = document.querySelector(".password-eye-container");
 // Get reference for the eye image
 const eyeImgEl = eyeContainerEl.getElementsByTagName("img")[0];
 
-// This will show/hide the eye
+// Show/hide the eye
 passwordEl.addEventListener("keyup", (e) => {
   e.target.value.length
     ? eyeContainerEl.classList.remove("hidden")
     : eyeContainerEl.classList.add("hidden");
 });
 
-// This will switch from the eye to the eye off and viceversa
+// Show/hide the password
 eyeContainerEl.addEventListener("click", () => {
   if (eyeImgEl.classList.contains("eye-off")) {
-    eyeImgEl.src = "../images/eye.png";
+    eyeImgEl.src = "images/eye.png";
     eyeImgEl.classList.remove("eye-off");
     passwordEl.type = "text";
   } else {
-    eyeImgEl.src = "../images/eye-off.png";
+    eyeImgEl.src = "images/eye-off.png";
     eyeImgEl.classList.add("eye-off");
     passwordEl.type = "password";
   }
